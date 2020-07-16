2c9a056f2e798ea3bd1b8de32eabce757dd18118
Laxman Budihal <laxmanbudihal0321@gmail.com>
2020-07-16 21:29:03 +0530

    Add files via upload

---
+++ Donot_Contain_3.c
@@ -0,0 +1,36 @@
+/*Question.
+
+Write a C Program to Count numbers that don’t contain 3
+
+Ans:
+*/
+#include <stdio.h>
+int count(int n) 
+{ 
+    
+    if (n < 3) 
+        return n; 
+    if (n >= 3 && n < 10) 
+       return n-1; 
+  
+    int po = 1; 
+    while (n/po > 9) 
+        po = po*10; 
+  
+   
+    int msd = n/po; 
+  
+    if (msd != 3) 
+     
+      return count(msd)*count(po - 1) + count(msd) + count(n%po); 
+    else
+      
+      return count(msd*po - 1); 
+} 
+  
+
+int main() 
+{ 
+    printf ("%d ", count(10)); 
+    return 0; 
+} 
