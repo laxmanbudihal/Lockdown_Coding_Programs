e2ee87001ed0e9536d04e8535819eebb6123da2a
Laxman Budihal <laxmanbudihal0321@gmail.com>
2020-07-16 21:45:11 +0530

    Add files via upload

---
+++ Split_Array.py
@@ -0,0 +1,24 @@
+# Python program to split array and move first
+# part to end.
+ 
+
+
+def splitArr(arr, n, k): 
+    for i in range(0, k): 
+        x = arr[0]
+        for j in range(0, n-1):
+            arr[j] = arr[j + 1]
+         
+        arr[n-1] = x
+         
+ 
+# main
+arr = [3,1,2]
+n = len(arr)
+position = 1
+ 
+splitArr(arr, n, position)
+ 
+for i in range(0, n): 
+    print(arr[i], end = ' ')
+ 
